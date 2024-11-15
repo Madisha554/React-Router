3e231eca4da28256c5710e045c37db4afe6faaed
Madiso Melese <157389082+Madisha554@users.noreply.github.com>
2024-11-15 14:50:34 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/home";
+import AboutUs from "./components/about";
+import Users from "./components/users";
+import Navbar from "./components/navbar"
+import NotFound from "./components/NotFound"
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/users" element={<Users />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import {Link} from 'react-router-dom'
+const NotFound = () => {
+  return (
+    <div>
+      <h1 className="mb-8"> 8:00<sub className='text-xl'>AM</sub> - Sleep Not Found</h1>
+      <Link to="/" className="text-2xl ">Go to Dorm please</Link>
+    </div>
+  )
+}
+
+export default NotFound
