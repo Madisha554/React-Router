d36b309caa4a2932a15540d73d4ddd0fecdac000
Madiso Melese <madishamadiso00@gmail.com>
2024-11-18 01:08:19 +0300

    Updates

---
+++ static/js/982.af3b3793.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrouter=self.webpackChunkrouter||[]).push([[982],{982:(e,s,l)=>{l.r(s),l.d(s,{default:()=>a});var t=l(475),r=l(579);const a=()=>(0,r.jsxs)("div",{children:[(0,r.jsxs)("h1",{className:"mb-8",children:[" 3:00",(0,r.jsx)("sub",{className:"text-xl",children:"AM"})," - Sleep Not Found"]}),(0,r.jsx)(t.N_,{to:"/",className:"text-2xl ",children:"Go to Dorm please"})]})}}]);
+//# sourceMappingURL=982.af3b3793.chunk.js.map
