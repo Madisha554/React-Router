123d6e7b4acbef52f8cfea494e093e3a71d407c6
Madiso Melese <madishamadiso00@gmail.com>
2024-12-24 16:35:04 +0300

    After deploy

---
+++ build/static/js/68.05e2ccbb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrouter=self.webpackChunkrouter||[]).push([[68],{68:(e,r,s)=>{s.r(r),s.d(r,{default:()=>t});var u=s(579);const t=()=>(0,u.jsx)("h1",{children:"About Us Page"})}}]);
+//# sourceMappingURL=68.05e2ccbb.chunk.js.map
